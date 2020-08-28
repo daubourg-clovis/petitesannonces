30aa8b387299bfb3e4ad3437532e411f51613ca2
vaness25 <vanessa.knorr@outlook.fr>
2020-08-28 10:30:12 +0200

    update

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php    
+require dirname(dirname(__FILE__))."/vendor/autoload.php";
+$router = new AltoRouter();
+
+// map homepage
+$router->map( 'GET', '/', function() {
+	\App\Homepage::homepage();
+});
+
+// map user details page
+$router->map( 'GET', '/user/[i:id]/', function( $id ) {
+	
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+
+
+
+?>
