9de55e97b16bb8822f50f4bd830bd3c1909ea1ce
vaness25 <vanessa.knorr@outlook.fr>
2020-08-28 10:34:41 +0200

    update

---
+++ application/class/Homepage.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+class Homepage{
+    public static function homepage(){
+        echo "coucou";
+      /*  "./templates/index.html.twig"; */
+      //  $loader = new \Twig\Loader\FilesystemLoader('../application/templates'); 
+
+    }
+}
