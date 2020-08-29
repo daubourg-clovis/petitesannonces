8e0435d008d6091a94c98f6545d8ea06e546f61c
vaness25 <vanessa.knorr@outlook.fr>
2020-08-29 16:10:13 +0200

    update

--- application/class/Homepage.php
+++ application/class/Homepage.php
@@ -1,7 +1,14 @@
 <?php
 namespace App;
+require_once('Db.php');
 class Homepage{
     public static function homepage(){
+        $db=new Db();
+        
+        $sql='SELECT * FROM annonce';
+        $annonces = $db->q($sql);
+        
+      var_dump($annonces);
        
         $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
         $twig = new \Twig\Environment($loader, [
@@ -13,5 +20,9 @@ class Homepage{
         echo $template->render(array(
             
         ));
+
+      
+
     }
+
 }
