d88aa288350512f773584771622006c2538ef2e2
vaness25 <vanessa.knorr@outlook.fr>
2020-08-31 13:49:26 +0200

    update

---
+++ application/class/Ajout.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace adding;
+
+class add {
+    
+} 
--- application/class/Homepage.php
+++ application/class/Homepage.php
@@ -5,7 +5,7 @@ class Homepage{
     public static function homepage(){
         $db=new Db();
         
-        $sql='SELECT * FROM annonce';
+        $sql='SELECT ann_unique_id, ann_description, ann_image_url, ann_date_validation, usr_nom, cat_libelle, usr_prenom, usr_courriel, usr_telephone, c.ID FROM annonce INNER JOIN categorie AS c ON ID_categorie = c.ID INNER JOIN utilisateur AS u ON ID_utilisateur = u.ID';
         $annonces = $db->q($sql);
         
       var_dump($annonces);
