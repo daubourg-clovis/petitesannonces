0d4926b0a4fa8905fa71101981686666619418c2
daubourg-clovis <daubourg.clovis@gmail.com>
2020-09-04 15:07:19 +0200

    Update - render twig

--- application/class/Detail.php
+++ application/class/Detail.php
@@ -13,7 +13,7 @@ class Detail{
     public static function detail($id){
         $db=new Db();
         
-        $sql='SELECT * FROM annonce WHERE ann_unique_id=:id';
+        $sql='SELECT ann_unique_id, ann_description, ann_image_url, ann_date_validation, usr_nom, cat_libelle, usr_prenom, usr_courriel, usr_telephone, c.ID, ann_titre, ann_prix FROM annonce INNER JOIN categorie AS c ON ID_categorie = c.ID INNER JOIN utilisateur AS u ON ID_utilisateur = u.ID WHERE ann_unique_id=:id';
        /*  $v1=':id';
         $v3=\PDO::PARAM_INT;
         $cond=[$v1, $id, $v3 ];
@@ -25,22 +25,23 @@ class Detail{
                     $stmt->bindParam(':id', $id, \PDO::PARAM_INT );
              
             $stmt->execute();
-            $annonce= $stmt->fetchAll();
+            $annonce = $stmt->fetchAll();
         
-        
+           var_dump($annonce);
        
-        $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
-        $twig = new \Twig\Environment($loader, [
-            'cache' => false,
-        ]);
-     
-      //  $loader = new \Twig\Loader\FilesystemLoader('../application/templates'); 
-      $template = $twig->load('detail.html.twig');
-        echo $template->render([
-            'annonce' => $annonce,
-            'basepath' => SERVER_URI,
-          ]);
+            
+                  $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
+                  $twig = new \Twig\Environment($loader, [
+                  'cache' => false,
+                    ]);
 
+            
+                  $template = $twig->load('detail.html.twig');
+                    echo $template->render([
+                      'annonce' => $annonce,
+                      'basepath' => SERVER_URI,
+                      
+                    ]);
       
 
     }
