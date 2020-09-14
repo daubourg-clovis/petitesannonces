bf1a2fe976d8fd1f3a9cbe5d8e62641536724ec4
vaness25 <vanessa.knorr@outlook.fr>
2020-09-14 10:32:14 +0200

    update

--- application/class/Mail.php
+++ application/class/Mail.php
@@ -3,12 +3,17 @@ namespace App;
 
 class Mail {
     // fonction qui créer le mail et l'envoie
-  static function mailto($mail,  $body){
+  static function mailto($mail, $body, $usr_prenom , $usr_nom){
+      // $mail=$_POST['email'];
         // Create the Transport
-$transport = (new \Swift_SmtpTransport('smtp.mailtrap.io', 25))
-->setUsername('95ae2b5f9c2ba1')
-->setPassword('6bb0407a7c9c33')
-// ->setStreamOptions(array('ssl' => array('allow_self_signed' => true, 'verify_peer' => false)))
+
+        $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+       ->setUsername('clovis.daubourg@gmail.com')
+       ->setPassword('LGBfMbK3etFC') 
+// $transport = (new \Swift_SmtpTransport('smtp.mailtrap.io', 587))
+// ->setUsername('95ae2b5f9c2ba1')
+// ->setPassword('6bb0407a7c9c33')
+//  ->setStreamOptions(array('ssl' => array('allow_self_signed' => true, 'verify_peer' => false)))
 ;
 
 // Create the Mailer using your created Transport
@@ -17,8 +22,8 @@ $mailer = new \Swift_Mailer($transport);
 // Create a message
 $message = (new \Swift_Message('annonces'))
  ->setContentType("text/html")
-->setFrom('vanessa.knorr@outlook.fr')
-->setTo(["vanessa.knorr@outlook.fr" => 'vanessa'])
+->setFrom('clovis.daubourg@gmail.com')
+->setTo([$mail => $usr_prenom." ".$usr_nom])
 ->setBody($body) 
 ;
 
